9e2da8ddac2b9c5fc24f2a49c4ab212928d6e178
Sabahat Nadeem <sabahatnadeem184@gmail.com>
2021-02-24 00:32:56 +0500

    report

--- authentication/forms.py
+++ authentication/forms.py
@@ -11,7 +11,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username/email",                
+                "placeholder" : "Username",                
                 "class": "form-control"
             }
         ))
